ccf71c1813fc41156a8576f75993be39210dab00
Manan shah <sc19mas@leeds.ac.uk>
2022-02-17 12:41:55 +0000

    login-page made more responsive

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -22,41 +22,41 @@ const Login: NextPage = () => {
   return (
     <div className='flex justify-start overflow-hidden'>
         <div className={`${Styles.Background} flex flex-col items-start justify-start h-screen`}>
-            <div className='p-5'>
-              <img src='./Logo.svg' alt='Logo' className='w-64' />
+            <div className='p-3 md:p-5'>
+              <img src='./Logo.svg' alt='Logo' className='max-w-full' />
             </div>
             <div className='flex justify-center items-center w-full relative py-3'>
               <div className={Styles.Circle}></div>
-              <object data='./LoginSVG.svg' className={`${Styles.Image} max-h-64`}/>
+              <object data='./LoginImage.png' className={`${Styles.Image} max-h-64`}/>
             </div>
             <div className={`flex flex-wrap mx-auto text-center mt-5 ${Styles.CarouselText}`}>
-              <h6 className='lg:text-lg mx-auto font-medium'>Gain visibility into your roles and control 
+              <h6 className='text-xs md:text-lg mx-auto font-medium'>Gain visibility into your roles and control 
                   over your permissions</h6>
             </div>
         </div>
         <div className='flex flex-col h-screen flex-1'>
-          <div className='p-5 ml-auto text-xs md:text-base'>
+          <div className='p-3 md:p-5 ml-auto text-xs md:text-base'>
             Need Help? <a className={`${Styles.Link} no-underline hover:${Styles.Link} cursor-pointer`}>Contact us.</a>
           </div>
           <div className='mt-5 flex flex-col justify-center items-center'>
-            <h6 className='text-base md:text-2xl'>Login to your account</h6>
+            <h6 className='text-sm md:text-2xl'>Login to your account</h6>
             <h6 className={`${Styles.Text} text-xs md:text-sm pt-2`}>Lorem Ipsum is simply dummy text of the printing</h6>
           </div>
           <div className='flex flex-col py-4 px-6 lg:px-16 xl:px-44'>
-            <label htmlFor='email' className='font-semibold'>Email ID* </label>
+            <label htmlFor='email' className='font-semibold text-xs md:text-base'>Email ID* </label>
             <Input 
             type='email' 
-            style={`${Styles.Input} mt-2`} 
+            style={`${Styles.Input} mt-2 w-40 text-xs md:text-base md:w-auto`} 
             placeholder = 'someone@gmail.com' 
             id='email'
             required={true}
             onChange = {(e) => setEmail(e.target.value)}
             value = {email}
             />
-            <label htmlFor='password' className='font-semibold pt-3'>Password* </label>
+            <label htmlFor='password' className='font-semibold pt-3 text-xs md:text-base'>Password* </label>
             <Input 
             type='password' 
-            style={`${Styles.Input} mt-2`} 
+            style={`${Styles.Input} mt-2 w-40 text-xs md:text-base md:w-auto`} 
             placeholder = '**************' 
             id='email'
             required={true}
@@ -74,7 +74,7 @@ const Login: NextPage = () => {
                 <FormControlLabel control={<></>} label={<a className={`text-xs cursor-pointer ${Styles.Link}`}>Forgot Password</a>} />
               </div>
             </div>
-            <Button className={`${Styles.Button} mt-3 text-lg font-semibold`} onClick = {() => handleLogin()}>Login</Button>
+            <Button className={`${Styles.Button} mt-3 sm:text-xs md:text-lg font-semibold`} onClick = {() => handleLogin()}>Login</Button>
           </div>
           <div className='mb-5 mt-auto ml-auto mr-auto'>
             <span className={`${Styles.Text} text-xs`}>@2022 EasyAudit. All Rights Reserved</span>
